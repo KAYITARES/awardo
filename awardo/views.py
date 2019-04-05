162963fa06b81a4fe7696b1fd3c19d950a7ea26d
MBOYIRE <mboyire14@gmail.com>
2019-04-05 14:06:26 +0200

    adding vote on views.py

--- awardo/views.py
+++ awardo/views.py
@@ -5,10 +5,17 @@ from .forms import ProfileForm,ProjectForm,VotesForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializer import ProjectSerializer,ProfileSerializer
+from rest_framework import status
+from .permissions import IsAdminOrReadOnly
+
 # Create your views here.
 # Create your views here.
 def welcome(request):
-    return render(request, 'index.html')
+    projects=Project.objects.all() 
+    return render(request, 'index.html',{"projects":projects})
 @login_required(login_url='/accounts/login/')
 def prof(request,id):
     user = User.objects.get(id = id)
@@ -42,3 +49,79 @@ def project(request):
     else:  
             form = ProjectForm()
     return render(request,'new-project.html',{"form":form})
+@login_required(login_url='/accounts/login/')
+def more(request, id):
+        project= Project.objects.get(id=id)
+        votes = Votes.objects.filter(project=project.id).all()
+
+        design=0
+        usability=0
+        content=0
+        number = len(votes)
+        for r in votes:
+                design+=round(r.design/number)
+                usability+=round(r.usability/number)
+                content+=round(r.content/number)
+
+        return render(request,'more.html',{"project":project,"votes":votes,"usability":usability,"design":design,"content":content})
+def vote(request, id):
+        current_user = request.user
+        post = Project.objects.get(id=id)
+        votes = Votes.objects.filter(project=post)
+        # print(comments)
+        form = VotesForm()
+        if request.method == 'POST':
+                vote = VotesForm(request.POST,request.FILES)
+                if vote.is_valid():
+                        design = vote.cleaned_data['design']
+                        usability= vote.cleaned_data['usability']
+                        content = vote.cleaned_data['content']
+                        rate = Votes(design = design,usability =usability,content=content,user=request.user,project=post)
+                        rate.save()
+                        return redirect("home")
+                    
+                
+        else:
+                return render(request, 'rate.html', {"form":form,'project':post,'user':current_user,'votes':votes})
+@login_required(login_url='/accounts/login/')
+def search(request):
+    
+    if 'project' in request.GET and request.GET['project']:
+        search_term = request.GET.get('project')
+        project_found = Project.search_project(search_term)
+        message = f'{search_term}'
+
+        return render(request, 'search.html',{'message':message, 'project_found':project_found})
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html',{"message":message})
+class ProfileList(APIView):
+    permission_classes = (IsAdminOrReadOnly,)
+    def get(self, request, format=None):
+        serializers = ProfileSerializer(data=request.data)
+        all_merch = Profile.objects.all()
+        serializers = ProfileSerializer(all_merch, many=True)
+        return Response(serializers.data)
+
+    def post(self, request, format=None):
+        serializers = ProfileSerializer(data=request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data, status=status.HTTP_201_CREATED)
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+   
+
+class ProjectList(APIView):
+    permission_classes = (IsAdminOrReadOnly,)
+    def get(self, request, format=None):
+        serializers = ProjectSerializer(data=request.data)
+        all_merch = Project.objects.all()
+        serializers = ProjectSerializer(all_merch, many=True)
+        return Response(serializers.data)
+
+    def post(self, request, format=None):
+        serializers = ProjectSerializer(data=request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data, status=status.HTTP_201_CREATED)
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
