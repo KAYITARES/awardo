f2d8d1f7da9ea163a675691655317229cc92935c
MBOYIRE <mboyire14@gmail.com>
2019-04-05 14:02:40 +0200

    adding vote url on url.py

--- awardo/urls.py
+++ awardo/urls.py
@@ -1,9 +1,18 @@
 from django.conf.urls import url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns=[
-    url('^$',views.welcome,name = 'home'),
+    url(r'^$',views.welcome,name = 'home'),
     url(r'^awardo/profile$',views.profile,name='displayProfile'),
     url(r'^awards/prof/(\d+)',views.prof,name="prof"),
     url(r'^project',views.project,name="project"),
+    url(r'^awards/more/(\d+)',views.more,name="more"),
+    url(r'^awards/vote/(\d+)',views.vote,name="vote"),
+    url(r'^search/', views.search,name='search_project'),
+    url(r'^api/merch/$', views.ProfileList.as_view()),
+    url(r'^api/project/$', views.ProjectList.as_view())
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
